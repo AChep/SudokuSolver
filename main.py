87a07ddadcda299ff8ae9e8eb5d9e30bf53e007b
Artem Chepurnoy <artemchep@gmail.com>
2015-04-03 02:07:40 +0300

    Add the copyright header to main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# Copyright (C) 2015 Artem Chepurnoy <artemchep@gmail.com>
+#
+# This script is published under the terms of the MIT license.
+# See http://opensource.org/licenses/mit-license.php
+
+# Python 3 is required
+
+
 from sudoku import Sudoku
 
 s = Sudoku()
